ede00c6cd79a3b513bf06581cc2e991cc950301a
01walid <hi@walid.dev>
2023-06-04 13:20:14 +0000

    chore(release): v0.0.0

--- cdktfgkeauth/jsii/jsii.go
+++ cdktfgkeauth/jsii/jsii.go
@@ -14,7 +14,7 @@ import (
 	cdktf      "github.com/hashicorp/terraform-cdk-go/cdktf/jsii"
 )
 
-//go:embed cdktf-gke-auth-1.1.0.tgz
+//go:embed cdktf-gke-auth-0.0.0.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -26,5 +26,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("cdktf-gke-auth", "1.1.0", tarball)
+	_jsii_.Load("cdktf-gke-auth", "0.0.0", tarball)
 }
